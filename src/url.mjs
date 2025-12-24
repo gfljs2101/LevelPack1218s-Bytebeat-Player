f7f269e48582e8e2e360f55ada61f7387ca29726
gfljs2101 <gfljs2100.user@gmail.com>
2025-12-25 03:47:07 +0800

    Fix url.mjs

--- src/url.mjs
+++ src/url.mjs
@@ -13,10 +13,10 @@ export function getCodeFromUrl(hash) {
 		} catch(err) {
 			console.error(`Couldn't load data from url: ${ err }`);
 		}
-	} else if(hash.startsWith('#v3b64') || hash.startsWith('#bytebeat2-') || hash.startsWith('#GFLJBeat2-') || hash.startsWith('#GFLJBeat3-') || hash.startsWith('#LevelPack-') || hash.startsWith('#EnBeat2-')) {
+	} else if(hash.startsWith('#v3b64') || hash.startsWith('#EnBeat2-')) {
 		try {
 			songData = inflateRaw(
-				Uint8Array.from(atob(hash.substring(hash.startsWith('#bytebeat2-') ? 9 : 6)), el => el.charCodeAt()), { to: 'string' });
+				Uint8Array.from(atob(hash.substring(hash.startsWith('#EnBeat2-') ? 9 : 6)), el => el.charCodeAt()), { to: 'string' });
 			if(songData.startsWith('{')) {
 				songData = JSON.parse(songData);
 				if(songData.formula) { // XXX: old format
@@ -36,8 +36,8 @@ export function getCodeFromUrl(hash) {
 
 export function getUrlFromCode(code, mode, sampleRate) {
 	const codeArr = deflateRaw(code);
-	// First byte is mode, next 4 bytes is sampleRate, then the code
-	const outputArr = new Uint8Array(5 + codeArr.length);
+	// First byte is mode, next 7 bytes is sampleRate, then the code
+	const outputArr = new Uint8Array(8 + codeArr.length);
 	outputArr[0] = ['Bytebeat', 'Signed Bytebeat', 'Floatbeat', 'Funcbeat', 'RAW', 'Signed RAW', 'FloatRAW'].indexOf(mode);
 	outputArr.set(new Uint8Array(new Float32Array([sampleRate]).buffer), 1);
 	outputArr.set(codeArr, 8);
