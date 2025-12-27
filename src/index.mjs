46d79db50f1a52b75c2a25dd4b7c00f8bd956d37
gfljs2101 <gfljs2100.user@gmail.com>
2025-12-27 19:41:30 +0800

    Change index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -551,6 +551,9 @@ globalThis.bytebeat = new class {
 		case 2: scope.colorChannels = [2, 0, 1]; break;
 		default: scope.colorChannels = [1, 0, 2];
 		}
+		if(scope.colorWaveform) {
+			scope.setStereoColors();
+		}
 	}
 	setSRDivisor(value) {
 		value = Number(value);
@@ -595,6 +598,7 @@ globalThis.bytebeat = new class {
 		}
 		ui.controlColorWaveform.value = value;
 		ui.controlColorWaveformInfo.innerHTML = scope.getColorTest('colorWaveform', value);
+		scope.setStereoColors();
 	}
 	setAudioSampleRate(value) {
 		if(value !== undefined) {
