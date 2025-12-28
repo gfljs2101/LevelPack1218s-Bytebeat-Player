2c723bbe42f0d33db1aa0b7e30b1846e059e5603
gfljs2101 <gfljs2100.user@gmail.com>
2025-12-28 14:56:45 +0800

    Change library.mjs

--- src/library.mjs
+++ src/library.mjs
@@ -40,6 +40,12 @@ export class Library {
 	}, libName) {
 		const notAllLib = libName !== 'all' && libName !== 'recent';
 		if(songs) {
+			songs.sort((a, b) => {
+				const da = new Date(a.date);
+				const db = new Date(b.date);
+				return da - db;
+			});
+
 			let songsStr = '';
 			const len = songs.length;
 			const maxVisible = 10;
