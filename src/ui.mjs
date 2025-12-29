c3218f1be477c09c3128f6456a966a80ef3e6dae
gfljs2101 <gfljs2100.user@gmail.com>
2025-12-29 16:40:10 +0800

    Change files

--- src/ui.mjs
+++ src/ui.mjs
@@ -108,7 +108,7 @@ export class UI {
 	}
 	setCodeSize(value) {
 		this.controlCodeSize.textContent =
-			`${ formatBytes(new Blob([value]).size, 1) } (${ formatBytes(window.location.href.length, 2) })`;
+			`${ formatBytes(new Blob([value]).size, 1) } (${ new Blob([value]).size }c) (${ formatBytes(window.location.href.length, 1) })`;
 	}
 	okAlert(message, callback){
 		this.okDialogText.innerText = message;
