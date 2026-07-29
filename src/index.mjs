ad6d3b672c662fd7c6c8c8dbd966bda58d511536
LevelPack1218 <gfljs2100.user@gmail.com>
2026-07-29 21:53:10 +0800

    Update index.mjs
    Signed-off-by: LevelPack1218 <gfljs2100.user@gmail.com>

--- src/index.mjs
+++ src/index.mjs
@@ -317,7 +317,10 @@ globalThis.bytebeat = new class {
 			if (mp3buf.length > 0) mp3Data.push(new Int8Array(mp3buf));
 
 			const blob = new Blob(mp3Data, { type: 'audio/mp3' });
-			this.saveData(blob, 'track.mp3');
+			ui.downloader.href = blob;
+			ui.downloader.download = "track.mp3";
+			ui.downloader.click();
+			setTimeout(() => window.URL.revokeObjectURL(blob));
 		});
 		this.audioGain.connect(mediaDest);
 	}
